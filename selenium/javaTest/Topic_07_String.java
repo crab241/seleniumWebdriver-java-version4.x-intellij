6e7c77c2d0c83726e99a96b9e36724231ac08227
crab241 <vubach24102004@gmail.com>
2025-12-08 21:08:51 +0700

    Add Topic 10 - Handle Button Selenium and Topic String with some usefull string function

---
+++ selenium/javaTest/Topic_07_String.java
@@ -0,0 +1,67 @@
+package javaTest;
+
+import org.testng.annotations.Test;
+
+public class Topic_07_String {
+
+    @Test
+    public void TC_01(){
+        String text = "Convert RGBA color value.";
+
+        // Kieerm tra chuỗi / Check string
+        // Tuyệt đối phân biệt hoa thường
+        System.out.println(text.equals("Convert RGBA color value."));
+        System.out.println(text.equals("Convert RGBA color value.."));
+
+        // Tuyệt đối k phân biệt hoa thường ( Tương đối )
+        System.out.println(text.equalsIgnoreCase("convert rgba color value."));
+        System.out.println(text.contains("Convert RGBA"));
+        System.out.println(text.contains("color"));
+        System.out.println(text.contains("value"));
+
+        System.out.println(text.startsWith("Convert"));
+        System.out.println(text.startsWith("Convert RGBA"));
+
+        System.out.println(text.endsWith("value."));
+        System.out.println(text.endsWith("color value."));
+
+
+        // Xử lí chuỗi
+        String[] textArray = text.split("RGBA");
+
+        for (String temp : textArray) {
+            System.out.println(temp);
+        }
+
+        // Gộp chung với 1 String khác
+        System.out.println(text.concat("format with alpha channel support"));
+
+        System.out.println(text.toLowerCase());
+        System.out.println(text.toUpperCase());
+
+        // Print chữ cái tại vị trí index
+        System.out.println(text.charAt(0));
+        System.out.println(text.charAt(1));
+        System.out.println(text.charAt(10));
+        System.out.println(text.charAt(text.length() - 1));
+        System.out.println(text.length());
+
+        // Similar to equals function
+        System.out.println(text.compareTo("Convert RGBA color value."));
+
+        // Trả về index của kí tự
+        System.out.println(text.indexOf("R"));
+
+        System.out.println(text.isBlank());
+        System.out.println(text.isEmpty());
+
+        // Thay thế giá trị target với giá trị replacement
+        System.out.println(text.replace("RGBA", "Hexa"));
+
+        text = "          \nConvert RGBA color value.\n         "
+        System.out.println(text);
+        System.out.println(text.length());
+        System.out.println(text.trim().length());
+
+    }
+}
---
+++ selenium/webdriver/Topic_10_Handle_Button.java
@@ -0,0 +1,57 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.Color;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_10_Handle_Button {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void TC_01_Fahasa(){
+        driver.get("https://www.fahasa.com/customer/account/create");
+
+        driver.findElement(By.cssSelector("li.popup-login-tab-login")).click();
+
+        By loginButton = By.cssSelector("button.fhs-btn-login");
+
+        // Verify button is disabled
+        Assert.assertFalse(driver.findElement(loginButton).isEnabled());
+
+        String loginButtonColorRGBA = driver.findElement(loginButton).getCssValue("background-color");
+        Color loginButtonColor = Color.fromString(loginButtonColorRGBA);
+        String loginButtonColorHexa = loginButtonColor.asHex().toUpperCase();
+
+        // Verify button color when being disabled
+        Assert.assertEquals(loginButtonColorHexa, "#000000");
+
+        driver.findElement(By.cssSelector("input#login_username")).sendKeys("0987654321");
+        driver.findElement(By.cssSelector("input#login_password")).sendKeys("Memaybeo@123");
+
+        // Verify if button is enabled
+        Assert.assertTrue(driver.findElement(loginButton).isEnabled());
+
+        // Verify button color when being enabled
+        Assert.assertEquals(Color.fromString(driver.findElement(loginButton).getCssValue("background-color")).asHex().toUpperCase(), "#C92127");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+}
