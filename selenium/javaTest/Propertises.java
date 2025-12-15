3582787c6127b9c98cda2035cc30bd9095ee4567
crab241 <vubach24102004@gmail.com>
2025-12-15 16:37:59 +0700

    Ass Topic 14: User Interactions Part 3 ( class code )

--- selenium/javaTest/Propertises.java
+++ selenium/javaTest/Propertises.java
@@ -3,6 +3,8 @@ package javaTest;
 public class Propertises {
     public static void main(String[] args) {
         String osName = System.getProperty("os.name");
+        String projectPath = System.getProperty("user.dir");
+        System.out.println(projectPath);
         System.out.println(osName);
     }
 }
--- selenium/webdriver/Template.java
+++ selenium/webdriver/Template.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -13,6 +14,7 @@ import java.time.Duration;
 public class Template {
     WebDriver driver;
     Actions actions;
+    JavascriptExecutor jsExecutor;
 
     @BeforeClass
     public void beforeClass(){
@@ -21,6 +23,8 @@ public class Template {
         option.addPreference("dom.webnotifications.enabled", false);
 
         driver = new FirefoxDriver(option);
+
+        jsExecutor = (JavascriptExecutor) driver;
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
