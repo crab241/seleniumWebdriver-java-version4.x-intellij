99fe8be266ab7203c96639758437d264ceb190d8
crab241 <vubach24102004@gmail.com>
2025-12-27 10:56:38 +0700

    Update Topic 22 Shadow DOM ( HW )

--- selenium/webdriver/Topic_22_Shadow_DOM.java
+++ selenium/webdriver/Topic_22_Shadow_DOM.java
@@ -100,6 +100,97 @@ public class Topic_22_Shadow_DOM {
 
     }
 
+    @Test
+    public void HW_TC_NestedShadowDOM_ShopPolymer(){
+        driver.get("https://shop.polymer-project.org/");
+
+        // Get the first shadow host
+        WebElement shoppAppShadowHost = driver.findElement(By.cssSelector("shop-app"));
+        SearchContext shopAppShadowRoot = shoppAppShadowHost.getShadowRoot();
+
+        // Get the second/ nested shadow DOM
+        WebElement shopHomeShadowHost = shopAppShadowRoot.findElement(By.cssSelector("shop-home"));
+        SearchContext shopHomeShadowRoot = shopHomeShadowHost.getShadowRoot();
+
+        shopHomeShadowRoot.findElement(By.cssSelector("shop-button>a[aria-label=\"Men's Outerwear Shop Now\"]")).click();
+        sleepInSecond(4);
+
+        // Re-get the first shadow host
+        shoppAppShadowHost = driver.findElement(By.cssSelector("shop-app"));
+        shopAppShadowRoot = shoppAppShadowHost.getShadowRoot();
+
+        // Get the shop-list shadow DOM
+        WebElement shopListShadowHost = shopAppShadowRoot.findElement(By.cssSelector("shop-list"));
+        SearchContext shopListShadowRoot = shopListShadowHost.getShadowRoot();
+
+        Assert.assertEquals(shopListShadowRoot.findElement(By.cssSelector("header>h1")).getText(), "Men's Outerwear");
+    }
+
+    @Test
+    public void TC_08_Github(){
+        driver.get("https://automationfc.github.io/shadow-dom");
+
+        // Get the shadow DOM that has sometext text
+        WebElement someTextShadowHost = driver.findElement(By.cssSelector("div#shadow_host"));
+        SearchContext someTextShadowRoot = someTextShadowHost.getShadowRoot();
+
+        // Verify text display
+        Assert.assertEquals(someTextShadowRoot.findElement(By.cssSelector("span.info")).getText(), "some text");
+
+        // Verify checkbox isn't selected
+        Assert.assertFalse(someTextShadowRoot.findElement(By.cssSelector("input[type='checkbox']")).isSelected());
+
+        // Get the shadow DOM that has nestedtext text
+        WebElement nestedTextShadowHost = someTextShadowRoot.findElement(By.cssSelector("div#nested_shadow_host"));
+        SearchContext nestedTextShadowRoot = nestedTextShadowHost.getShadowRoot();
+
+        // Verify text display
+        Assert.assertEquals(nestedTextShadowRoot.findElement(By.cssSelector("div#nested_shadow_content>div")).getText(), "nested text");
+
+    }
+
+    @Test
+    public void HW_TC_09_Books_Pwakit(){
+        driver.get("https://books-pwakit.appspot.com/");
+
+        // Necessary sleep
+        sleepInSecond(3);
+
+        // Get the first shadow DOM layer
+        WebElement bookAppShadowHost = driver.findElement(By.cssSelector("book-app"));
+        SearchContext bookAppShadowRoot = bookAppShadowHost.getShadowRoot();
+
+        bookAppShadowRoot.findElement(By.cssSelector("input")).sendKeys("Harry Potter");
+
+        // Get a nested shadow DOM inside the first one
+        WebElement bookInputShadowHost = bookAppShadowRoot.findElement(By.cssSelector("book-input-decorator"));
+        SearchContext bookInputShadowRoot = bookInputShadowHost.getShadowRoot();
+
+        bookInputShadowRoot.findElement(By.cssSelector("div.icon")).click();
+        // Necessary sleep
+        sleepInSecond(4);
+
+        // Need to re-asign the value of the first Shadow DOM because it is a new page
+        bookAppShadowHost = driver.findElement(By.cssSelector("book-app"));
+        bookAppShadowRoot = bookAppShadowHost.getShadowRoot();
+
+        // Get another nested shadow DOM inside the first one
+        WebElement bookExploreShadowHost = bookAppShadowRoot.findElement(By.cssSelector("book-explore"));
+        SearchContext bookExploreShadowRoot = bookExploreShadowHost.getShadowRoot();
+
+        // Store all the return book results into a list
+        // Each one of the book item is also a Shadow Host
+        List<WebElement> bookItemShadowHosts = bookExploreShadowRoot.findElements(By.cssSelector("section>ul>li>book-item"));
+
+        // Loop through all the book Items
+        for (WebElement bookItem : bookItemShadowHosts) {
+            // Get into the shadow DOM inside each of the li
+            SearchContext bookItemShadowRoot = bookItem.getShadowRoot();
+            System.out.println(bookItemShadowRoot.findElement(By.cssSelector("div.title-container>h2.title")).getText());
+        }
+
+    }
+
     private static void sleepInSecond(long timeWait) {
         try {
             Thread.sleep(timeWait * 1000);
