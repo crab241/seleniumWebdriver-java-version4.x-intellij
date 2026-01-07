322b92a4c727de62843ac9b0277dd51acaf94cf4
crab241 <vubach24102004@gmail.com>
2026-01-07 22:02:16 +0700

    Update topic 28 : Wait Part IV, Explicit Wait ( HW )

--- selenium/webdriver/Topic_28_Wait_Part_IV_Explicit.java
+++ selenium/webdriver/Topic_28_Wait_Part_IV_Explicit.java
@@ -126,6 +126,97 @@ public class Topic_28_Wait_Part_IV_Explicit {
                 By.xpath("//a[contains(@class, 'item_open') and text()='" + moonFileName + "']"))).isDisplayed();
     }
 
+    @Test
+    public void HW_TC_04_Only_Explicit_Invisible(){
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#start>button"))).click();
+
+        // Wait for loading icon to disappear
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div#loading")));
+
+        // Wait and Verify text
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#finish>h4"))).getText(), "Hello World!");
+    }
+
+    @Test
+    public void HW_TC_05_OnlyExplicit_Invisible(){
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#start>button"))).click();
+
+        // Wait for text to appear and verify it
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#finish>h4"))).getText(), "Hello World!");
+    }
+
+    @Test
+    public void HW_TC_06_Explicit(){
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+
+        // Wait for date picker to appear
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#ctl00_ContentPlaceholder1_Panel1")));
+
+        // Verify text before picking date
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("span#ctl00_ContentPlaceholder1_Label1"))).getText(), "No Selected Dates to display.");
+
+        // Pick a date
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td//a[text()='7']"))).click();
+
+        // Wait for loading icon to disappear
+        // Using CssSelector: div#ctl00_ContentPlaceholder1_RadAjaxLoadingPanel1ctl00_ContentPlaceholder1_RadCalendar1>div.raDiv
+        // Another locator: div:not([style='display:none;'])>div[class='raDiv']
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div:not([style='display:none;'])>div[class='raDiv']"))));
+
+        // Wait and verify text after selected date
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("span#ctl00_ContentPlaceholder1_Label1"))).getText(), "Wednesday, January 7, 2026");
+    }
+
+    @Test
+    public void HW_TC_07_Explicit(){
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        driver.get("https://gofile.io/?t=uploadFiles");
+
+        // Wait for all the loading icons to disappear
+        explicitWait.until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(By.cssSelector("div.animate-spin"))));
+
+        // Upload files
+        explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[type='file']"))).
+                sendKeys(astronautFilePath + "\n" + galaxyFilePath + "\n" + moonFilePath);
+
+        // Wait for the all the progress bars and loading icons to finish and disappear
+        explicitWait.until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(By.cssSelector("div.progress-container"))));
+        explicitWait.until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(By.cssSelector("div.animate-spin"))));
+
+        // Get the Uploaded files link
+        String uploadLink = explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a.linkSuccessCard"))).getDomProperty("href");
+
+        // Navigate to the uploaded link
+        driver.get(uploadLink);
+        // or we can just click the link
+        //explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a.linkSuccessCard"))).click();
+
+        // Wait for all the loading icons to disappear
+        explicitWait.until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(By.cssSelector("div.animate-spin"))));
+
+        // Verify uploaded files
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[contains(@class, 'item_open') and text()='" + astronautFileName + "']"))).isDisplayed());
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[contains(@class, 'item_open') and text()='" + galaxyFileName + "']"))).isDisplayed());
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[contains(@class, 'item_open') and text()='" + moonFileName + "']"))).isDisplayed());
+
+    }
+
     @AfterClass
     public void afterClass(){
         driver.quit();
