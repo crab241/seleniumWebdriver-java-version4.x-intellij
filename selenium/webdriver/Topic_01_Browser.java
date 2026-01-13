6c2688535015a590bef4e3a39649f2e3f61a007e
crab241 <vubach24102004@gmail.com>
2026-01-13 15:38:46 +0700

    Update: Exercise Browser and Login

--- selenium/webdriver/Topic_01_Browser.java
+++ selenium/webdriver/Topic_01_Browser.java
@@ -1,14 +1,32 @@
 package webdriver;
 
-import graphql.relay.Edge;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class Topic_01_Browser {
     WebDriver driver;
+    WebDriverWait explicitWait;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new FirefoxDriver();
+
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
 
     @Test
     public void TC_01_Chrome(){
@@ -32,4 +50,85 @@ public class Topic_01_Browser {
         driver.get("https://demo.nopcommerce.com/");
         driver.quit();
     }
+
+    @Test
+    public void Redo_TC_01_Verify_URL(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account'] "))).click();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//h1[contains(text(), 'Login or Create an Account')]")));
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Create an Account']"))).click();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text()='Create an Account']")));
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+
+    }
+
+    @Test
+    public void Redo_TC_02_Verify_Title(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account'] "))).click();
+
+        Assert.assertFalse(explicitWait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//title[text()='Customer Login']"))).isDisplayed());
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Create an Account']"))).click();
+
+        Assert.assertFalse(explicitWait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//title[text()='Create New Customer Account']"))).isDisplayed());
+    }
+
+    @Test
+    public void Redo_TC_03_Navigate_Function(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account'] "))).click();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Create an Account']"))).click();
+
+        explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//title[text()='Create New Customer Account']")));
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+
+        driver.navigate().back();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//h1[contains(text(), 'Login or Create an Account')]")));
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+
+        driver.navigate().forward();
+
+        explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//title[text()='Create New Customer Account']")));
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+
+    }
+
+    @Test
+    public void Redo_TC_04_GetPageSource(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account'] "))).click();
+
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//h1[contains(text(), 'Login or Create an Account ')]"))).isDisplayed());
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Create an Account']"))).click();
+
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//h1[contains(text(), 'Create an Account')]"))).isDisplayed());
+    }
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+
 }
--- selenium/webdriver/Topic_04_Login_Exercise.java
+++ selenium/webdriver/Topic_04_Login_Exercise.java
@@ -3,18 +3,32 @@ package webdriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_04_Login_Exercise {
+import java.time.Duration;
+import java.util.Random;
 
+public class Topic_04_Login_Exercise {
     WebDriver driver;
+    WebDriverWait explicitWait;
+    String randomEmail = "johnnycage" + new Random().nextInt(9999) + "@gmail.com";
 
     @BeforeClass
     public void beforeClass(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        driver = new ChromeDriver(options);
+
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
     }
 
@@ -82,6 +96,66 @@ public class Topic_04_Login_Exercise {
         Thread.sleep(2000);
         // li.error-msg>ul>li can be raplace with li-error-msg span
         Assert.assertEquals(driver.findElement(By.cssSelector("li.error-msg>ul>li")).getText(), "Invalid login or password.");
+    }
+
+    @Test
+    public void Redo_TC_01_Login_With_Empty_Email_Pass(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account']"))).click();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#send2"))).click();
+
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div#advice-required-entry-email"))).getText(), "This is a required field.");
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div#advice-required-entry-pass"))).getText(), "This is a required field.");
+    }
+
+    @Test
+    public void Redo_TC_02_Login_With_Invalid_Email(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account']"))).click();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#email"))).sendKeys("123434234@12312.123123");
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#pass"))).sendKeys("123456");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#send2"))).click();
+
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div#advice-validate-email-email"))).getText(), "Please enter a valid email address. For example johndoe@domain.com.");
+    }
+
+    @Test
+    public void Redo_TC_03_Login_With_Invalid_Password(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account']"))).click();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#email"))).sendKeys(randomEmail);
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#pass"))).sendKeys("123");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#send2"))).click();
+
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div#advice-validate-password-pass"))).getText(), "Please enter 6 or more characters without leading or trailing spaces.");
+
+    }
+
+    @Test
+    public void Redo_TC_04_Login_With_Incorrect_Email_Pass(){
+        driver.get("http://live.techpanda.org/");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='footer'] a[title='My Account']"))).click();
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#email"))).sendKeys("automation@gmail.com");
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#pass"))).sendKeys("123123123");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#send2"))).click();
+
+        Assert.assertEquals(explicitWait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("li.error-msg span"))).getText(), "Invalid login or password.");
     }
 
     @AfterClass
